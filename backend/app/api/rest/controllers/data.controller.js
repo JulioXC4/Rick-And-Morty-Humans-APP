dac79694744d40825974d3c26b5f9f0347af0c74
JulioXC4 <danielc4nt@gmail.com>
2023-10-17 10:29:02 -0500

    Agregado parametros a la consulta del endpoint

--- backend/app/api/rest/controllers/data.controller.js
+++ backend/app/api/rest/controllers/data.controller.js
@@ -2,34 +2,51 @@ const axios = require('axios');
 
 const getHumans = async (req, res) => {
     try {
+      let allHumans = [];
+      let page = 1;
+      let totalPages = 1;
+  
+      while (page <= totalPages) {
         const response = await axios.post('https://rickandmortyapi.com/graphql', {
-          query: `{
-            characters(filter: { species: "Human" }) {
-              results {
-                id
-                name
-                species
-                status
-                type
-                gender
-                origin {
+          query: `
+            {
+              characters(page: ${page}, filter: { species: "Human" }) {
+                info {
+                  pages
+                }
+                results {
+                  id
                   name
-                }
-                location {
+                  species
+                  status
+                  type
+                  gender
+                  origin {
                     name
+                  }
+                  location {
+                    name
+                  }
+                  image
                 }
-                image
               }
             }
-          }
           `,
         });
-    
-        res.status(200).json(response.data.data.characters.results);
-      } catch (error) {
-        console.error('Error al obtener datos de la API de GraphQL', error);
-        res.status(500).json({ error: 'Error al obtener datos de la API de GraphQL' });
+  
+        const data = response.data.data.characters;
+  
+        allHumans.push(...data.results);
+  
+        page++;
+        totalPages = data.info.pages;
       }
-}
+  
+      res.status(200).json(allHumans);
+    } catch (error) {
+      console.error('Error al obtener datos de la API de GraphQL', error);
+      res.status(500).json({ error: 'Error al obtener datos de la API de GraphQL' });
+    }
+  };
 
 module.exports = { getHumans }
